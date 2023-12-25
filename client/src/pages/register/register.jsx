4cb5160f04951bb243f30552e752586c80c12b26
Ayush Jha <ashujha301@gmail.com>
2023-12-25 18:55:37 +0530

    login and register page

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,5 +1,25 @@
+
+import './register.css';
+
 const Register = () => {
-    return ( <>Register Page</> );
-}
- 
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
 export default Register;
+
