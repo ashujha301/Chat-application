9924a3439a424f96de5497d57815fa2afa98e4eb
Ayush Jha <ashujha301@gmail.com>
2024-01-24 16:06:06 +0530

    Root page height:fix

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,18 +1,21 @@
-import { Navbar, Nav } from "react-bootstrap";
-import "./navbar.css"; // Import the regular CSS file
+import "./navbar.css";
 
 const MyNavbar = () => {
   return (
     <div className="navbar-container p-3">
-      <Navbar className="rounded navbar" variant="dark">
-        <Navbar.Brand className="brand mr-auto">ChatApp</Navbar.Brand>
-        <Nav className="nav mx-auto">
-          <Nav.Link href="#hello">Hello Ayush</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#logout">Logout</Nav.Link>
-        </Nav>
-      </Navbar>
+      <div className="rounded navbar" id="main-navbar">
+        <div className="brand mr-auto">ChatApp</div>
+        <div className="nav mx-auto">
+          <div className="nav-link" href="#hello">
+            Hello Ayush
+          </div>
+        </div>
+        <div>
+          <div className="nav-link" href="#logout">
+            Logout
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
