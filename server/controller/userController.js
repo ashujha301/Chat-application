7bd4e46a46dc90f596980d7a294f020f450460f8
Ayush Jha <ashujha301@gmail.com>
2024-01-24 16:39:16 +0530

    finduser api

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,64 +3,77 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
-const createToken = (_id)=>{
-    const jwtkey = process.env.JWT_KEY;
-    return jwt.sign({ _id },jwtkey,{expiresIn: "3d"});
+const createToken = (_id) => {
+  const jwtkey = process.env.JWT_KEY;
+  return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
 };
 
 const registerUser = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-        let user = await userModel.findOne({ email });
+    let user = await userModel.findOne({ email });
 
-        if (user) {
-            return res.status(400).json({ error: 'Email already exists' });
-        }
-
-        if (!validator.isEmail(email)) {
-            return res.status(400).json({ error: 'Email must be valid' });
-        }
-
-        if (!validator.isStrongPassword(password)) {
-            return res.status(400).json({ error: 'Password must be strong' });
-        }
-
-        user = new userModel({ name, email, password });
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-
-        await user.save();
-
-        const token = createToken(user._id);
-
-        res.status(200).json({ _id: user._id, name, email, token });
-    } catch (error) {
-        console.error('Error in registerUser:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (user) {
+      return res.status(400).json({ error: "Email already exists" });
     }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ error: "Email must be valid" });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({ error: "Password must be strong" });
+    }
+
+    user = new userModel({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ _id: user._id, name, email, token });
+  } catch (error) {
+    console.error("Error in registerUser:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-const loginUser = async (req,res)=>{
-    const { email, password } = req.body;
-   try {
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
     const user = await userModel.findOne({ email });
-    if(!user){
-        return res.status(400).json({ error:'User not found'});
-        }
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
 
-    if(!await bcrypt.compare(password, user.password)){
-        return res.status(401).json({ error: "Invalid Password" })
-        }
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Invalid Password" });
+    }
     //delete the password from the response body before sending it to the client side
     delete user.password;
-    const token=createToken(user._id);
-    res.status(200).json({token, name:user.name , email:email});
-   } catch (error) {
-    return res.status(500).json({error});
-   }
-            
-}
+    const token = createToken(user._id);
+    res.status(200).json({ token, name: user.name, email: email });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
 
-module.exports = { registerUser , loginUser };
+const findUser = async (req, res) => {
+  try {
+    const userid = req.params.id;
+    let user = await userModel.findById(userid);
+    if (!user) {
+      res.status(400).json("user not found");
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = { registerUser, loginUser, findUser };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {registerUser, loginUser} = require("../controller/userController");
+const {registerUser, loginUser, findUser} = require("../controller/userController");
 const router = express.Router();
 
 router.post("/register" , registerUser);
 router.post("/login", loginUser);
+router.get("/finduser/:id", findUser);
 
 
 module.exports = router;
