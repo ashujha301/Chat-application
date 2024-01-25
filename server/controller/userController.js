0aa258f7596ff98f1fbf4157e2434b426076399e
Ayush Jha <ashujha301@gmail.com>
2024-01-25 23:54:14 +0530

    get all user api

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -76,4 +76,17 @@ const findUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser, findUser };
+const getUser = async (req, res) => {
+  try {
+    
+    const users = await userModel.find();
+    res.status(200).json(users);
+    
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+
+
+module.exports = { registerUser, loginUser, findUser , getUser};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const {registerUser, loginUser, findUser} = require("../controller/userController");
+const {registerUser, loginUser, findUser, getUser} = require("../controller/userController");
 const router = express.Router();
 
 router.post("/register" , registerUser);
 router.post("/login", loginUser);
 router.get("/finduser/:id", findUser);
+router.get("/", getUser);
 
 
 module.exports = router;
